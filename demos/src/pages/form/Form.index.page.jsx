dc495fea6d98fa7dcf2bdc73a15c731e156aca2a
Aiden <bokjaeoh@gmail.com>
2023-09-27 14:32:26 +0900

    Add demo pages

--- demos/src/pages/form/Form.index.page.jsx
+++ demos/src/pages/form/Form.index.page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
-import { TextFieldDemoPage } from '.'
+import { FormDemoPage, InputExtraDemoPage, TextFieldDemoPage } from '.'
 
 /**
  * React component that provides an index page for form related demo pages.
@@ -12,6 +12,8 @@ import { TextFieldDemoPage } from '.'
 const FormIndexPage = () => {
   return (
     <Routes>
+      <Route element={<FormDemoPage />} path="form" />
+      <Route element={<InputExtraDemoPage />} path="inputextra" />
       <Route element={<TextFieldDemoPage />} path="textfield" />
       <Route element={<h1>Form Index Page</h1>} path="*" />
     </Routes>
